cbb50254c46a15fdda3c5a1f9688d00c40b5f906
Arjun S <arjunjs.m8@gmail.com>
2025-04-15 20:01:57 +0530

    second commit

--- ML_nlp/nl.py
+++ ML_nlp/nl.py
@@ -1,10 +1,15 @@
-from nltk.tokenize import sent_tokenize
+from nltk.tokenize import sent_tokenize , word_tokenize
 #mport nltk
 #nltk.download('punkt_tab')
 
-paragraph ='''Hi Goood mornign, ahdaslfasfj ksfnskflks fjsfjsf.'''
+paragraph ='''The sun's dipped below the horizon, painting the sky with hues of amber and rose. 
+A soft breeze rustled the leaves, carrying with it the scent of jasmine. 
+In that quiet moment, the world seemed to pause, wrapped in golden stillness.'''
 
 
 sent = sent_tokenize(paragraph)
 
 print(sent)
+
+words_token = word_tokenize(paragraph)
+print(words_token)
